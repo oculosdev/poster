795cd8ddaef7ff892332587a2b333fba6196eb0d
Sven Thorsen <sven@oculos.no>
2018-11-15 15:01:35 +0100

    chat working

---
+++ js/initfirebase.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDUOVjE9WrQE7G5y5Kr_u8gbbtFYT6SOVU",
+    authDomain: "oc-poster.firebaseapp.com",
+    databaseURL: "https://oc-poster.firebaseio.com",
+    projectId: "oc-poster",
+    storageBucket: "oc-poster.appspot.com",
+    messagingSenderId: "800169037581"
+  };
+  firebase.initializeApp(config);
--- js/poster.js
+++ js/poster.js
@@ -1,4 +1,8 @@
 
+
+var viewModel = {
+};
+
 function Post(text, userId, timestamp) {
     this.text = text;
     this.userId = userId;
@@ -31,9 +35,18 @@ function renderPost(post) {
 
 function createPost() {
     var text = document.getElementById("inputText");
-    var post = new Post(text.value, "user-id", moment.utc().valueOf());
+    var post = new Post(text.value, viewModel.userId, moment.utc().valueOf());
 
-    renderPost(post);
+    getCollection("messages")
+        .add(JSON.parse(JSON.stringify(post)))
+        .then(function(docRef) {
+            post.id = docRef.id;
+            //renderPost(post);
+        })
+        .catch(function(error) {
+            console.log(error);
+            return error;
+        });
 
     text.value = "";
 }
@@ -48,9 +61,75 @@ function enterHit(event) {
     }
 }
 
+function setCurrentUser(user) {
+    viewModel.userId = user.uid;
+    viewModel.userName = user.email;
+
+    document.getElementById("userName").innerText = user.email;
+}
+
+function getDb() {
+    let db = firebase.firestore();
+    const settings = {/* your settings... */ timestampsInSnapshots: true};
+    db.settings(settings);
+    return db;
+}
+
+function getCollection(name) {
+    return getDb().collection(name);
+}
+
+function setUpSession() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            let user = firebase.auth().currentUser;
+
+            setCurrentUser(user);
+            
+            const usersColl = getCollection("users");
+
+            const userDocRef = usersColl.doc(user.uid);
+            userDocRef.get()
+            .then(function(doc) {
+                return userDocRef.set({
+                    id: user.uid,
+                    email: user.email,
+                    status: 1
+                });
+            }).catch(function(error) {
+                console.log(error);
+                return error;    
+            });
+        } else {
+            window.location = "/index.html";
+        }
+    });
+}
+
+
+function loadMessages() {
+    const postsQuery = getCollection("messages")
+        .orderBy('timestamp', 'asc');
+
+        postsQuery.onSnapshot(function(snapshot) {
+            snapshot.docChanges().forEach(function(change) {
+                if(!snapshot.size) {
+                    return;
+                }
+
+                if (change.type === 'added') {
+                    renderPost(change.doc.data());
+                }
+            });
+        });
+}
+
 function start(){
     document.getElementById("postButton").addEventListener("click", postButtonClick);
     document.getElementById("inputText").addEventListener("keyup", enterHit);
+
+    setUpSession();
+    loadMessages();
 }
 
 window.addEventListener("load", start);
